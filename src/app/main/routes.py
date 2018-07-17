7520e5f79f044c919e1c7d104500d35790d107d0
Joao Antunes <jmfantunes@gmail.com>
2018-07-17 01:22:47 +0100

    Hypotenuse Project Page

--- src/app/main/routes.py
+++ src/app/main/routes.py
@@ -6,3 +6,8 @@ from flask import render_template
 @app.route('/')
 def home():
     return render_template('main/home_page.html')
+
+
+@app.route('hypotenuse/')
+def hypotenuse_page():
+    return render_template('main/hypotenuse_page.html')
